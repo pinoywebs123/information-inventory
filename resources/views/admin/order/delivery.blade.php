8246166d4aa1b737f0dd1662b5c6c22a72db08d9
Morls <moravel752@gmail.com>
2019-03-13 22:54:11 +0800

    Adding tables to all admin dashboards pages

--- resources/views/admin/order/delivery.blade.php
+++ resources/views/admin/order/delivery.blade.php
@@ -1,5 +1,17 @@
 @extends('admin.template')
 
 @section('contents')
-	<h1>DELIVERY</h1>
+	<div>
+		<table class="table" id="table">
+			<thead>
+				<th>Customer Name</th>
+				<th>Product Name</th>
+				<th>Price</th>
+				<th>Quantity</th>
+				<th>Date</th>
+				<th>Status</th>
+				<th>Actions</th>
+			</thead>
+		</table>
+	</div>
 @endsection
--- resources/views/admin/report/list.blade.php
+++ resources/views/admin/report/list.blade.php
@@ -1,5 +1,24 @@
 @extends('admin.template')
 
 @section('contents')
-	<h1>REPORT LIST</h1>
+	<div class="text-right">
+		<form>
+			<input type="date" name="start_date" required="">
+			<input type="date" name="end_date" required="">
+			<button type="submit" class="btn btn-danger btn-xs">Generate Report</button>
+		</form>
+	</div>
+	<div>
+		<table class="table" id="table">
+			<thead>
+				<th>Customer Name</th>
+				<th>Product Name</th>
+				<th>Price</th>
+				<th>Quantity</th>
+				<th>Date</th>
+				<th>Status</th>
+				<th>Actions</th>
+			</thead>
+		</table>
+	</div>
 @endsection
